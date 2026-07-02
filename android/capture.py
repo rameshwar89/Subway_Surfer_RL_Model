66bc160530ed642bfb4a7f8234f625118c110e30
Rameshwar_Sahu <rameshwarsahu627@gmail.com>
2026-07-03 00:12:13 +0530

    scrcpy setup done -adb call slow replacement by scrcpy needed

--- android/capture.py
+++ android/capture.py
@@ -1,23 +1,63 @@
-import subprocess
+import threading
+import time
 
-import cv2
-import numpy as np
+import scrcpy
 
 
 class ScreenCapture:
 
+    def __init__(self):
+
+        self.latest_frame = None
+        self.lock = threading.Lock()
+
+        self.client = scrcpy.Client(
+            max_width=720,
+            bitrate=8000000,
+            max_fps=30,
+        )
+
+        self.client.add_listener(
+            scrcpy.EVENT_FRAME,
+            self._on_frame,
+        )
+
+        self.client.start(
+            threaded=True,
+            daemon_threaded=True,
+        )
+
+        print("Starting Scrcpy Stream...")
+
+        while True:
+
+            with self.lock:
+                if self.latest_frame is not None:
+                    break
+
+            time.sleep(0.02)
+
+        print("Scrcpy Stream Ready!")
+
+    def _on_frame(self, frame):
+
+        if frame is None:
+            return
+
+        with self.lock:
+            self.latest_frame = frame
+
     def grab(self):
 
-        process = subprocess.Popen(
-            ["adb", "exec-out", "screencap", "-p"],
-            stdout=subprocess.PIPE,
-        )
+        while True:
 
-        image_bytes = process.stdout.read()
+            with self.lock:
 
-        image = cv2.imdecode(
-            np.frombuffer(image_bytes, np.uint8),
-            cv2.IMREAD_COLOR,
-        )
+                if self.latest_frame is not None:
+                    return self.latest_frame.copy()
 
-        return image
+            time.sleep(0.001)
+
+    def close(self):
+
+        self.capture.close()
